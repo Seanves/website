c8acb71a2ad73c69a2590a88c57b0bdd1b9ff547
Seanves <matviy1998@gmail.com>
2024-06-06 19:21:26 +0300

    add darkening background when sidebar open

--- src/main/java/demo/security/repositories/UserRepository.java
+++ src/main/java/demo/security/repositories/UserRepository.java
@@ -2,10 +2,7 @@ package demo.security.repositories;
 
 import demo.security.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import java.util.List;
 import java.util.Optional;
 
 
@@ -14,20 +11,4 @@ public interface UserRepository extends JpaRepository<User,Integer> {
 
     Optional<User> findByUsername(String username);
     User findById(int id);
-
-    List<User> findAll();
-
-    @Override
-    User save(User user);
-
-//    @Modifying
-//    @Query("UPDATE User p SET p.birthYear = :birthYear WHERE p.id = :id")
-//    void updateBirthYearById(int id, int birthYear);
-
-//    void updateColor(User person, String color);
-//
-//    @Modifying
-//    @Transactional
-//    @Query("UPDATE User p SET p.password = :newPassword WHERE p.id = :personId")
-//    void updatePasswordById(int personId, String newPassword);
 }
